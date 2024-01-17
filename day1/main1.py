2ba8b739e6a038c3d8c2ebbc0c528bb215681a3d
Mr-Lii <MrLii@gmail.com>
2024-01-17 16:53:14 +0000

    day one

---
+++ day1/main1.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation")
+print('String Concatenateion is done with the "+" sign.')
+print('e.g. printHello " + "world")')
+print("New lines can be created with a backslash and n.")
