84aee9200456da13b18d6e3ecc7ba5662c13a0a4
Mr-Lii <MrLii@gmail.com>
2024-01-18 09:19:08 +0000

    day one complete

---
+++ day1/Project_of_the_day(Band_Name_Gen).py
@@ -0,0 +1,4 @@
+print("Hello, Welcome to Rock and Roll bsnk Nicki Generator :)")
+city = input("Could you please tell me the City you grew up: \n")
+pet = input("What is the name of a pet you had or know: \n")
+print("Your band Nick name should be:", city+pet+"s")
