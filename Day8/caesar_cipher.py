199e57a92196d9bd07f592648f430c4ba2cc0fad
Mr-Lii <leetamparega@gmail.com>
2024-03-15 08:33:36 +0000

    encode completed

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -16,7 +16,7 @@ def encrypt(text, shift):
             pos = pos - 26
         texts[count] = alphabet[pos]
         count = count + 1
-    print("".join(texts))
+    print(f"The encoded text is {"".join(texts)}")
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
